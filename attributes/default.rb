19224c35f2ff8c1c51a5bc489205219d6f91ed18
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2015-02-08 00:48:24 -0600

    Add nginx config, unicorn config, default git repo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 default["compass_webapp"]["database"] = "compass_webapp_db"
 default["compass_webapp"]["db_username"] = "compass_webapp"
+default["app"] = {
+  :name => "compass_webapp",
+  :web_dir => "/var/www/compass_webapp"
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -14,13 +14,73 @@ include_recipe "postgresql::server"
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
+package "vim"
+package "git"
+
+user "#{node.user.name}"
+
 rbenv_ruby "2.1.5" do
-  force true
   global true
 end
 
-nginx_site "default" do
+rbenv_gem "bundler" do
+  ruby_version "2.1.5"
+end
+
+# execute 'bundle install' do
+#   cwd "#{node.app.web_dir}/app"
+#   not_if 'bundle check' # This is not run inside /myapp
+# end
+
+directory node.app.web_dir do
+  owner node.user.name
+  mode "0755"
+  recursive true
+end
+
+directory "#{node.app.web_dir}/public" do
+  owner node.user.name
+  mode "0755"
+  recursive true
+end
+
+directory "#{node.app.web_dir}/logs" do
+  owner node.user.name
+  mode "0755"
+  recursive true
+end
+
+template "#{node.nginx.dir}/sites-available/#{node.app.name}.conf" do
+  source "webapp/nginx.conf.erb"
+  mode "0644"
+end
+
+# It's import to have this git resource before
+# the template resource below the references unicorn.rb
+git "#{node.app.web_dir}/app" do
+  repository "git://github.com/patrickspencer/compass-webapp"
+  reference "master"
+  action :sync
+end
+
+template "#{node.app.web_dir}/app/config/unicorn.rb" do
+  source "webapp/unicorn.rb.erb"
+  mode "0644"
+end
+
+
+file "#{node.nginx.dir}/sites-enabled/default" do
+  action :delete
+end
+
+nginx_site "#{node.app.name}.conf" do
   enable true
+end
+
+cookbook_file "#{node.app.web_dir}/public/index.html" do
+  source "index.html"
+  mode "0755"
+  owner node.user.name
 end
 
 postgresql_database 'compass_db_prod' do
